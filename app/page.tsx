86b9e8b26df4ca8fdf05c51007a0c32081a7126c
Boburov Shukurullo <boburovdev@gmail.com>
2025-07-21 17:00:29 +0500

    all commits push

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 "use client";
-import { useEffect } from "react";
 import Home from "./components/Home";
 
 export default function Page() {
-  useEffect(() => {
-    alert("useEffect ishga tushdi ✅");
-
-    if (typeof window === "undefined") {
-      alert("❌ window aniqlanmagan (probably SSR)");
-      return;
-    }
-  }, []);
-
   return (
     <div>
       <Home />
